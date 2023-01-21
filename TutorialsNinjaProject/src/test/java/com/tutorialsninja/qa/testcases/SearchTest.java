f314d4f6bba1582130a805b11a31490a6ab3d5bb
Admin <Admin@192.168.1.7>
2023-01-21 23:19:34 +0530

    Updated comment in SearchTest.java

--- TutorialsNinjaProject/src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
+++ TutorialsNinjaProject/src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
@@ -11,6 +11,8 @@ import com.tutorialsninja.qa.base.Base;
 import com.tutorialsninja.qa.pages.HomePage;
 import com.tutorialsninja.qa.pages.SearchPage;
 
+//Updated comment
+
 public class SearchTest extends Base {
 
 	SearchPage searchPage;
